c14a50078d4884d77e7f9dc3356cb148be23c61e
Pranto mollick <prantomollick01@gmail.com>
2023-10-28 19:39:47 +0600

    building api for creating issues

---
+++ app/api/issues/route.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { z } from 'zod';
+
+import prisma from '@/prisma/client';
+
+const createIssueSchema = z.object({
+  title: z.string().min(1).max(255),
+  description: z.string().min(1),
+});
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+
+  console.log(body);
+
+  const validation = createIssueSchema.safeParse(body);
+
+  console.log(validation);
+  if (!validation.success) {
+    return NextResponse.json(validation.error.errors, { status: 400 });
+  }
+
+  const newIssue = await prisma.issue.create({
+    data: { title: body.title, description: body.description },
+  });
+
+  console.log(newIssue);
+
+  return NextResponse.json(newIssue, { status: 201 });
+}
