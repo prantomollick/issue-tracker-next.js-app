e889bae4aef9dd227f464a790355fc494f890caa
Pranto Mollick <prantocse01@gmail.com>
2023-12-17 02:07:05 +0600

    building zod validation in creating issue api route

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { useSelectedLayoutSegments } from 'next/navigation';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import React from 'react';
--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,16 +1,21 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { type NextRequest, NextResponse } from 'next/server';
+import { z } from 'zod';
 
 import prisma from '@/prisma/client';
-import { createIssueSchema } from '@/app/validationSchemas';
+// import { createIssueSchema } from '@/app/validationSchemas';
+
+const createIssueSchema = z.object({
+  title: z.string().min(1).max(255),
+  description: z.string().min(1),
+});
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
 
   const validation = createIssueSchema.safeParse(body);
 
-  console.log(validation);
   if (!validation.success) {
-    return NextResponse.json(validation.error.format(), { status: 400 });
+    return NextResponse.json(validation.error.errors, { status: 400 });
   }
 
   const newIssue = await prisma.issue.create({
