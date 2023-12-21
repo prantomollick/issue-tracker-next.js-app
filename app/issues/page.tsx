cb5dce2fff303c43e3f83d53af96ed920665a8e7
Pranto Mollick <prantocse01@gmail.com>
2023-12-22 00:38:02 +0600

    add loading skeletons

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -2,19 +2,18 @@ import React from 'react';
 import Link from 'next/link';
 import { Button, Table } from '@radix-ui/themes';
 import prisma from '@/prisma/client';
-import IssueStatusBadge from '../components/issueStatusBadge';
+import IssueStatusBadge from '@/app/components/IssueStatusBadge';
+import delay from 'delay';
+import IssueActions from './IssueActions';
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
 
+  await delay(2000);
+
   return (
     <div>
-      <div className="mb-5">
-        <Button>
-          <Link href="issues/new">New Issue</Link>
-        </Button>
-      </div>
-
+      <IssueActions />
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
