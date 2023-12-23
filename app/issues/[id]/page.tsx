a1a4fb8891d60603af967df6e554c50c2f3f2da9
Pranto Mollick <prantocse01@gmail.com>
2023-12-23 19:47:10 +0600

    styling issue details page

---
+++ app/issues/[id]/page.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import prisma from '@/prisma/client';
+import { Card, Flex, Heading, Text } from '@radix-ui/themes';
+import { notFound } from 'next/navigation';
+import IssueStatusBadge from '@/app/components/IssueStatusBadge';
+
+interface IssueDetailPageProps {
+  params: { id: string };
+}
+
+const IssueDetailPage: React.FC<IssueDetailPageProps> = async ({ params }) => {
+  // if (typeof params.id !== 'number') notFound();
+
+  const issue = await prisma.issue.findUnique({
+    where: {
+      id: parseInt(params.id),
+    },
+  });
+
+  if (!issue) {
+    notFound();
+  }
+
+  return (
+    <div>
+      <Heading>{issue.title}</Heading>
+      <Flex gap="3" my="2">
+        <IssueStatusBadge status={issue.status} />
+        <Text>{issue.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card>
+        <p>{issue.description}</p>
+      </Card>
+    </div>
+  );
+};
+
+export default IssueDetailPage;
