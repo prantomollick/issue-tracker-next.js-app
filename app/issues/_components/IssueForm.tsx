a1e881949b13fccd393410ba55deeb01829d0e26
Pranto Mollick <prantocse01@gmail.com>
2023-12-27 23:59:15 +0600

    update issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -37,7 +37,11 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
   const onSubmit = handleSubmit(async (data) => {
     try {
       setIsSubmitting(true);
-      await axios.post('/api/issues', data);
+      if (issue) {
+        await axios.patch('/api/issues/' + issue.id, data);
+      } else {
+        await axios.post('/api/issues', data);
+      }
       setIsSubmitting(false);
       router.push('/issues');
     } catch (error) {
@@ -72,7 +76,8 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
         />
         <ErrorMessage>{errors.description?.message}</ErrorMessage>
         <Button disabled={isSubmitting}>
-          Submit New Issue {isSubmitting && <Spinner />}
+          {issue ? 'Update Issue' : 'Submit New Issue'}
+          {isSubmitting && <Spinner />}
         </Button>
       </form>
     </div>
