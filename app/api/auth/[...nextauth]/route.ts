5c5b8ff1c011db71192c0f6104b6d64caba8a9f2
Pranto Mollick <prantocse01@gmail.com>
2024-01-06 12:02:32 +0600

    configure google provider and themes color scheme mode Nextauth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,19 @@
 import GoogleProvider from 'next-auth/providers/google';
 import NextAuth from 'next-auth';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import prisma from '@/prisma/client';
 
 const handler = NextAuth({
+  adapter: PrismaAdapter(prisma) as any,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  theme: {
+    colorScheme: 'light',
+  },
 });
 
 export { handler as GET, handler as POST };
