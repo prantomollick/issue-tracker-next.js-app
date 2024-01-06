d43632c65428dbb1138418d47e0a39427b4643af
Pranto Mollick <prantocse01@gmail.com>
2024-01-06 13:52:39 +0600

    refactoring the navbar inside navbar.tsx file and create new tailwind utilities class global.scss file

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -17,8 +17,25 @@ import {
 } from '@radix-ui/themes';
 
 const NavBar = () => {
+  return (
+    <nav className="mb-5 border-b px-5 py-3">
+      <Container>
+        <Flex justify="between">
+          <Flex gap="3" align="center">
+            <Link href="/">
+              <AiFillBug />
+            </Link>
+            <NavLinks />
+          </Flex>
+          <AuthStatus />
+        </Flex>
+      </Container>
+    </nav>
+  );
+};
+
+const NavLinks = () => {
   const currentPath = usePathname();
-  const { status, data: session } = useSession();
 
   const links = [
     { label: 'Dashboard', href: '/' },
@@ -26,59 +43,59 @@ const NavBar = () => {
   ];
 
   return (
-    <nav className="mb-5 h-14 border-b px-5">
-      <Container>
-        <Flex justify="between" align="center">
-          <Flex gap="3" align="center">
-            <Link href="/">
-              <AiFillBug />
-            </Link>
-            <ul className="flex space-x-6">
-              {links.map((link) => (
-                <li key={link.href}>
-                  <Link
-                    className={classNames({
-                      'text-zinc-900': link.href === currentPath,
-                      'text-zinc-500': link.href !== currentPath,
-                      'transition-colors hover:text-zinc-800': true,
-                    })}
-                    href={link.href}
-                  >
-                    {link.label}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </Flex>
-          <Box>
-            {status === 'authenticated' && (
-              <DropdownMenu.Root>
-                <DropdownMenu.Trigger>
-                  <Avatar
-                    src={session.user!.image!}
-                    size="3"
-                    radius="full"
-                    fallback="?"
-                  />
-                </DropdownMenu.Trigger>
-                <DropdownMenu.Content>
-                  <DropdownMenu.Label>
-                    <Text size="2">{session.user!.email}</Text>
-                  </DropdownMenu.Label>
-                  <DropdownMenu.Item>
-                    <Link href="/api/auth/signout">Log out</Link>
-                  </DropdownMenu.Item>
-                </DropdownMenu.Content>
-              </DropdownMenu.Root>
-            )}
+    <ul className="flex space-x-6">
+      {links.map((link) => (
+        <li key={link.href}>
+          <Link
+            className={classNames({
+              'nav-link': true,
+              '!text-zinc-900': link.href === currentPath,
+            })}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
-            {status === 'unauthenticated' && (
-              <Link href="/api/auth/signin">Log in</Link>
-            )}
-          </Box>
-        </Flex>
-      </Container>
-    </nav>
+const AuthStatus = () => {
+  const { status, data: session } = useSession();
+
+  if (status === 'loading') return null;
+
+  if (status === 'unauthenticated') {
+    return (
+      <Link className="nav-link" href="/api/auth/signin">
+        Log in
+      </Link>
+    );
+  }
+
+  return (
+    <Box>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Avatar
+            src={session!.user!.image!}
+            size="3"
+            radius="full"
+            fallback="?"
+            className="cursor-pointer"
+          />
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content>
+          <DropdownMenu.Label>
+            <Text size="2">{session!.user!.email}</Text>
+          </DropdownMenu.Label>
+          <DropdownMenu.Item>
+            <Link href="/api/auth/signout">Log out</Link>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Box>
   );
 };
 
