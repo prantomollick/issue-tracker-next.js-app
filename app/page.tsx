6f51679e541ebd7205d4a4954cea955135bbc741
Pranto Mollick <prantocse01@gmail.com>
2024-01-10 17:10:08 +0600

    build the issue summary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
+import prisma from '@/prisma/client';
+import IssueSummary from './IssueSummary';
 import LatestIssues from './LatestIssues';
 
-export default function Home() {
-  return <LatestIssues />;
+export default async function Home() {
+  const open = await prisma.issue.count({
+    where: { status: 'OPEN' },
+  });
+
+  const closed = await prisma.issue.count({
+    where: { status: 'CLOSED' },
+  });
+
+  const inProgress = await prisma.issue.count({
+    where: { status: 'IN_PROGRESS' },
+  });
+
+  return <IssueSummary open={open} inProgress={inProgress} closed={closed} />;
 }
