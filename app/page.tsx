74a8011072d2afba3e2d19ebb024f4f9a54b82b3
Pranto Mollick <prantocse01@gmail.com>
2024-01-10 13:01:36 +0600

    build the layout of the pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Pagination from './components/Pagination';
+
 export default function Home() {
-  return <div>hello world</div>;
+  return <Pagination itemCount={100} pageSize={10} currentPage={2} />;
 }
